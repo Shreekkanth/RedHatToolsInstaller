90b40e11d58188654d3d5d879c7314de024baa6d
Shadd Gallegos <SGallego@redhat.com>
2020-05-06 07:51:30 -0600

    Add files via upload

---
+++ ANSIBLETOWER_3.6.4-1-INSTALLER.sh
@@ -0,0 +1,450 @@
+#!/bin/bash
+# This installer will 
+#1.verify you are root 
+#2.Check you are connected to the internet.
+#3.Provide a breif overview of what the tool is.
+#4.Help the end user register with Red Hat if not already done.
+#5.Take of some prep stuff install  shut off firewall and selinux and install pip prior to install.    
+#6.Enable required repos for OS and Ansible Tower.
+#7.Upgrades the OS.
+#8.Installs the dependencies from the bundle forces them to requirement levels listed in bundle.
+#9.Installs Tower. (Queries user for tower password) 
+#10. Gives the end user the option to enable firewall and selinux.
+
+echo -ne "\e[8;45;120t"
+reset
+if [ "$(whoami)" != "root" ]
+then
+echo "This script must be run as root - if you do not have the credentials please contact your administrator"
+exit
+else
+echo 'ANSIBLE-TOWER 3.6.4 INSTALLER FOR RHEL 7.x AND RHEL 8.x
+FOR SETTING UP A SIMPLE SINGLE NODE CONFIGURATION FOR P.O.C.'
+read -p "To Continue Press [Enter] or use Ctrl+c to exit the installer"
+
+#-------------------------
+function ANSIBLETOWERTXT {
+#-------------------------
+reset
+HNAME=$(hostname)
+echo " "
+echo " "
+echo " "
+echo "
+ANSIBLE-TOWER BASE HARDWARE REQUIREMENTS
+
+1. Ansible-Tower will require a RHEL subscription and an Ansible Tower License.
+Please register and download your lincense at http://www.ansible.com/tower-trial
+
+2. Hardware requirement depends, however whether 
+   it is a KVM or physical-Tower will require atleast 1 node with:
+
+        Min Storage 35GB
+        Directorys  Recommended
+        /boot  1024MB
+        /swap  8192MB
+        / Rest of drive
+
+
+        Min RAM 4096
+        Min CPU 2 (4 Reccomended)
+
+3. Network
+    Connection to the internet so the installer can download the required packages"
+echo " "
+echo " "
+echo " "
+read -p "Press [Enter] to continue"
+reset
+echo " "
+echo "
+
+REQUIREMENTS CONTINUED
+
+4. For this POC you must have a RHN User ID and password with entitlements
+   to channels below. (item 6)
+
+5. Install ansible tgz will be downloaded and placed into the FILES directory created by the sript on the host machine:
+
+6. This install was tested with:
+        * RHEL_7.x in a KVM environment.
+        * Ansible Tower 3.6.4 https://releases.ansible.com/ansible-tower/setup-bundle/ansible-tower-setup-bundle-latest.el7.tar.gz
+        * Red Hat subscriber channels:
+            rhel-7-server-ansible-2.8-rpms
+            rhel-7-server-extras-rpms
+            rhel-7-server-optional-rpms
+            rhel-7-server-rpms
+            https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+        * RHEL_8.x in a KVM environment.
+        * Ansible Tower 3.6.4 https://releases.ansible.com/ansible-tower/setup-bundle/ansible-tower-setup-bundle-latest.el8.tar.gz
+        * Red Hat subscriber channels:
+            ansible-2.9-for-rhel-8-x86_64-rpms
+            rhel-8-for-x86_64-appstream-rpms
+            rhel-8-for-x86_64-baseos-rpms
+            rhel-8-for-x86_64-supplementary-rpms
+            rhel-8-for-x86_64-optional-rpms
+            https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+
+URL Resources 
+        http://www.ansible.com
+        https://www.ansible.com/tower-trial
+        http://docs.ansible.com/ansible-tower/latest/html/quickinstall/index.html"
+echo " "
+echo " "
+read -p "If you have met all of the minimum requirements from above please Press [Enter] to continue"
+echo " "
+reset
+}
+
+#-----------------------------
+function ANSIBLECHECKCONNECT {
+#-----------------------------
+echo "********************************************"
+echo "Verifying the server can get to the internet"
+echo "********************************************"
+wget -q --tries=10 --timeout=20 --spider http://redhat.com
+if [[ $? -eq 0 ]]; then
+    echo "Online: Continuing to Install"
+            sleep 2
+else
+    echo "Offline"
+    echo "This script requires access to 
+              the network to run please fix your settings and try again"
+    sleep 2
+    exit 1
+fi
+}
+
+#-------------------------
+function ANSIBLEREGISTER {
+#-------------------------
+    echo "****************************"
+    echo "Registering system if needed"
+    echo "****************************"
+        subscription-manager status | awk -F ':' '{print $2}'|grep Current > /dev/null
+        status=$?
+        if test $status -eq 1
+        then
+            echo "System is not registered, Please provide Red Hat CDN username and password when prompted"
+            subscription-manager register --auto-attach
+    
+        else
+            echo "System is registered with Red Hat or Red Hat Satellite, Continuing!"
+    sleep 2 
+    fi
+echo " "
+echo " "
+}
+
+#-------------------------------
+function ANSIBLEPREPFORINSTALL {
+#-------------------------------
+mkdir ~/Downloads
+cd ~/Downloads
+echo "***************************************************************************"
+echo "SET SELINUX TO PERMISSIVE FOR THE INSTALL AND CONFIG OF Ansible Tower 3.6.4"
+echo "***************************************************************************"
+    sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+    setenforce 0
+    service firewalld stop
+echo " "
+echo " "
+echo "*******************"
+echo "FIRST DISABLE REPOS"
+echo "*******************"
+    subscription-manager repos --disable "*"
+    yum-config-manager --disable "*"
+echo " "
+echo " "
+}
+
+#----------------------------
+function ANSIBLESYSTEMREPOS {
+#----------------------------
+grep -q -i "release 7." /etc/redhat-release
+status=$?
+if test $status -eq 0
+then
+    echo "*******************"
+    echo "ENABLE REPOS RHEL7 "
+    echo "*******************"
+    yum -q list installed epel &>/dev/null && echo "epel is installed" || yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm --skip-broken
+    subscription-manager repos --enable --enable rhel-7-server-extras-rpms --enable rhel-7-server-optional-rpms --enable rhel-7-server-supplementary-rpms --enable rhel-server-rhscl-7-rpms --enable rhel-7-server-rpms --enable rhel-7-server-ansible-2.9-rpms
+    yum clean all
+    rm -rf /var/cache/yum
+    yum-config-manager --setopt=\*.skip_if_unavailable=1 --save \*
+echo " "
+echo " "
+sleep 2
+    echo "********************************"
+    echo "CHECKING AND INSTALLING PACKAGES"
+    echo "********************************"
+    yum -q list installed wget &>/dev/null && echo "wget is installed" || yum install -y wget --skip-broken --noplugins
+    yum -q list installed python3-pip-wheel &>/dev/null && echo "wgpython3-pip-wheel is installed" || yum install -y python3-pip-wheel --skip-broken --noplugins
+    yum -q list installed python3-pip &>/dev/null && echo "python3-pip is installed" || yum install -y python3-pip --skip-broken --noplugins
+    yum -q list installed platform-python-pip &>/dev/null && echo "platform-python-pip is installed" || yum install -y platform-python-pip --skip-broken --noplugins
+    yum -q list installed yum-utils &>/dev/null && echo "yum-utils is installed" || yum install -y yum-util* --skip-broken --noplugins
+    yum -q list installed dialog &>/dev/null && echo "dialog is installed" || yum localinstall -y dialog --skip-broken --noplugins
+    yum -q list installed bash-completion-extras &>/dev/null && echo "bash-completion-extras" || yum install -y bash-completion-extras --skip-broken --noplugins
+    yum -q list installed dconf &>/dev/null && echo "dconf" || yum install -y dconf* --skip-broken --noplugins
+    yum -q list installed ansible-tower-cli &>/dev/null && echo "ansible-tower-cli is installed" || dnf install -y https://releases.ansible.com/ansible-tower/cli/ansible-tower-cli-el7.repo --skip-broken --noplugins --best --allowerasing
+
+    yum-config-manager --disable epel
+echo " "
+echo " "
+    
+else
+    echo '*******************'
+    echo 'ENABLE REPOS RHEL 8' 
+    echo '*******************'
+    yum -q list installed epel &>/dev/null && echo "epel is installed" || dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm --skip-broken
+    subscription-manager repos --enable ansible-2.8-for-rhel-8-x86_64-rpms --enable rhel-8-for-x86_64-appstream-rpms --enable rhel-8-for-x86_64-baseos-rpms --enable rhel-8-for-x86_64-supplementary-rpms 
+    dnf clean all
+    rm -rf /var/cache/dnf
+echo " "
+echo " "
+sleep 2
+    echo "********************************"
+    echo "CHECKING AND INSTALLING PACKAGES"
+    echo "********************************"
+    yum -q list installed yum-utils &>/dev/null && echo "yum-utils is installed" || dnf install -y yum-util* --skip-broken --noplugins --best --allowerasing
+    yum -q list installed wget &>/dev/null && echo "wget is installed" || dnf install -y wget --skip-broken --noplugins  --best --allowerasing
+    yum -q list installed python3-pip-wheel &>/dev/null && echo "wgpython3-pip-wheel is installed" || dnf install -y python3-pip-wheel --skip-broken --noplugins --best --allowerasing
+    yum -q list installed python3-pip &>/dev/null && echo "python3-pip is installed" || dnf install -y python3-pip --skip-broken --noplugins --best --allowerasing
+    yum -q list installed platform-python-pip &>/dev/null && echo "platform-python-pip is installed" || dnf install -y platform-python-pip --skip-broken --noplugins --best --allowerasing
+    yum -q list installed dconf &>/dev/null && echo "dconf" || dnf install -y dconf* --skip-broken --noplugins --best --allowerasing
+    yum -q list installed dnf-utils &>/dev/null && echo "dnf-utils is installed" || dnf install -y dnf-utils --skip-broken --noplugins --best --allowerasing
+    yum -q list installed ansible-tower-cli &>/dev/null && echo "ansible-tower-cli is installed" || dnf install -y https://releases.ansible.com/ansible-tower/cli/ansible-tower-cli-el8.repo --skip-broken --noplugins --best --allowerasing
+    yum-config-manager --disable epel
+echo " "
+echo " "
+sleep 2
+fi
+}
+
+#-----------------------------
+function ANSIBLELINUXUPGRADE {
+#-----------------------------
+grep -q -i "release 7." /etc/redhat-release
+status=$?
+if test $status -eq 0
+then
+    echo "*******************"
+    echo "Upgrade RHEL7 "
+    echo "*******************"
+        yum upgrade -y
+echo " "
+echo " "
+else
+    echo "*******************"
+    echo "Upgrade RHEL8 "
+    echo "*******************"
+        dnf upgrade --best --allowerasing -y
+echo " "
+echo " "
+fi
+}
+
+#---------------------------
+function CloudRequirements {
+#---------------------------
+    echo '*********************************************'
+    echo 'Installing Cloud Requirements (Ignore Errors)'
+    echo '*********************************************'
+        source /var/lib/awx/venv/ansible/bin/activate
+        umask "0022"
+        yum install python3-pip 
+        pip install pip --upgrade
+        pip install six
+        pip install six --upgrade
+        pip freeze | grep six
+echo " "
+echo " "
+        pip install awscli
+        pip install awscli --upgrade 
+        pip freeze | grep awscli
+echo " "
+echo " "
+        for i in $(pip freeze | grep azure | awk -F '=' '{print $1}') ; do pip install "$i" --upgrade  ; done
+        pip install azure
+        pip install azure  --upgrade
+        pip install azure-common
+        pip install azure-common --upgrade
+        pip install azure-mgmt-authorization
+        pip install azure-mgmt-authorization --upgrade
+        pip install azure-mgmt
+        pip install azure-mgmt --upgrade 
+        pip freeze | grep azure
+echo " "
+echo " "
+        pip install boto
+        pip install boto --upgrade 
+        pip install boto3
+        pip install boto3 --upgrade 
+        pip install botocore
+        pip install botocore --upgrade
+        pip freeze | grep boto
+echo " "
+echo " "
+        pip install pywinrm
+        pip install pywinrm --upgrade
+        pip freeze | grep pywinrm
+echo " "
+echo " "
+        rpm -e --nodeps requests
+        pip install requests
+        pip install requests --upgrade
+        pip freeze | grep requests
+echo " "
+echo " "
+        pip install requests-credssp
+        pip install requests-credssp --upgrade
+        pip freeze | grep requests-credssp
+echo " "
+echo " "
+        pip install PyVmomi
+        pip install PyVmomi --upgrade
+        pip freeze | grep PyVmomi
+    deactivate
+echo " "
+echo " "
+}
+
+#-----------------------------
+function ANSIBLEINSTALLTOWER {
+#-----------------------------
+grep -q -i "release 7." /etc/redhat-release
+status=$?
+if test $status -eq 0
+then
+    echo '****************************************************************'
+    echo 'Getting, Expanding, and installing Ansible Tower 3.6.4 for RHEL7'
+    echo '****************************************************************'
+        mkdir ~/Downloads
+        cd ~/Downloads
+        wget https://releases.ansible.com/ansible-tower/setup-bundle/ansible-tower-setup-bundle-latest.el7.tar.gz
+        tar -zxvf ansible-tower-setup-bundle-latest.el7.tar.gz
+        yum localinstall -y ~/Downloads/ansible-tower-setup-bundle-3.6.4-1/bundle/el7/repos/ansible-tower-dependencies/*.rpm 
+        cd ~/Downloads/ansible-tower-setup-bundle-3.6.4-1/
+        sleep 2
+        reset
+echo " "
+echo " " 
+    echo 'What would you like your default Ansible Tower user "admin" password to be?'
+        read ADMINPASSWORD
+        export $ADMINPASSWORD
+        sed -i 's/admin_password='"''"'/admin_password='"'"$ADMINPASSWORD"'"'/g' ~/Downloads/ansible-tower-setup-bundle-3.6.4-1.el7/inventory
+        sed -i 's/pg_password='"''"'/pg_password='"'"$ADMINPASSWORD"'"'/g' ~/Downloads/ansible-tower-setup-bundle-3.6.4-1.el7/inventory
+        sed -i 's/rabbitmq_password='"''"'/rabbitmq_password='"'"$ADMINPASSWORD"'"'/g' ~/Downloads/ansible-tower-setup-bundle-3.6.4-1.el7/inventory
+        sh setup.sh
+        sleep 5
+echo " "
+echo " "
+else
+    echo '****************************************************************'
+    echo 'Getting, Expanding, and installing Ansible Tower 3.6.4 for RHEL8'
+    echo '****************************************************************'
+        mkdir ~/Downloads
+        cd ~/Downloads
+        wget https://releases.ansible.com/ansible-tower/setup-bundle/ansible-tower-setup-bundle-latest.el8.tar.gz
+        tar -zxvf ansible-tower-setup-bundle-latest.el8.tar.gz
+        dnf localinstall -y ~/Downloads/ansible-tower-setup-bundle-3.6.4-1/bundle/el8/repos/ansible-tower-dependencies/*.rpm --best --allowerasing
+        cd ~/Downloads/ansible-tower*/
+        sleep 2
+        reset
+echo " "
+echo " "        
+    echo 'What would you like your default Ansible Tower user "admin" password to be?'
+        read ADMINPASSWORD
+        export $ADMINPASSWORD
+        sed -i 's/admin_password='"''"'/admin_password='"'"$ADMINPASSWORD"'"'/g' ~/Downloads/ansible-tower*/inventory 
+        sed -i 's/pg_password='"''"'/pg_password='"'"$ADMINPASSWORD"'"'/g' ~/Downloads/ansible-tower*/inventory
+        sed -i 's/rabbitmq_password='"''"'/rabbitmq_password='"'"$ADMINPASSWORD"'"'/g' ~/Downloads/ansible-tower*/inventory
+echo " "
+echo " " 
+        sh setup.sh
+        sleep 5
+echo " "
+echo " "
+fi
+}
+
+DEFAULTVALUE=y
+NMESSAGE="Disabled"
+FMESSAGE="PLEASE ENTER Y or N"
+COUNTDOWN=10
+#-------------------------
+function ANSIBLESELINUX {
+#-------------------------
+clear
+echo '*******'
+echo 'SELinux'
+echo '*******'
+echo 'If you do not know what selinux is please visit
+https://www.redhat.com/en/topics/linux/what-is-selinux.
+It is Red Hats position that SELinux should be enabled unless 
+your enterprise dictates you disable SELinux on your systems.
+The default answer is yes'
+echo " "
+read -n1 -p "Would you like to ENABLE SELinux? Y/n " INPUT
+INPUT=${INPUT:-$DEFAULTVALUE}
+if  [ "$INPUT" = "y" -o "$INPUT" = "Y" ] ;then
+    sed -i 's/SELINUX=permissive/SELINUX=enforcing/g' /etc/selinux/config
+    setenforce 1
+    getenforce
+    sleep 3
+elif [ "$INPUT" = "n" -o "$INPUT" = "N" ] ;then
+echo -e "\n$NMESSAGE\n"
+else
+echo -e "\n$FMESSAGE\n"
+REQUEST
+fi
+}
+
+#-------------------------
+function ANSIBLEFIREWALL {
+#-------------------------
+clear
+echo " "
+echo '********'
+echo 'Firewall'
+echo '********'
+echo ''
+echo 'The ports used by Ansible Tower and its services are:
+          
+          80, 443 (normal Tower ports)
+          22 (ssh)
+          5432 (database instance - if the database is installed on 
+               an external instance, needs to be opened to the tower instances)'
+echo " "
+read -n1 -p "Would you like to ENABLE Firewall? Y/n " INPUT
+INPUT=${INPUT:-$DEFAULTVALUE}
+if  [ "$INPUT" = "y" -o "$INPUT" = "Y" ] ;then
+firewall-cmd --permanent \
+--add-port="80/tcp" --add-port="443/tcp" \
+--add-port="22/tcp" --add-port="5432/tcp"
+    sleep 3
+
+elif [ "$INPUT" = "n" -o "$INPUT" = "N" ] ;then
+service firewalld stop
+chkconfig firewalld off
+echo -e "\n$NMESSAGE\n"
+else
+echo -e "\n$FMESSAGE\n"
+REQUEST
+fi
+}  
+
+
+ANSIBLECHECKCONNECT
+ANSIBLETOWERTXT
+ANSIBLEREGISTER
+ANSIBLEPREPFORINSTALL
+ANSIBLESYSTEMREPOS
+ANSIBLELINUXUPGRADE
+ANSIBLEINSTALLTOWER
+#CloudRequirements
+ANSIBLESELINUX
+ANSIBLEFIREWALL
+fi
